5e91c170d78a96be348caf1c7f2f8492a766b745
Filipp <sxexesx@gmail.com>
2023-08-29 12:56:50 +0300

    Update build_array.go

--- array_permutation/build_array.go
+++ array_permutation/build_array.go
@@ -1,15 +1,16 @@
 package array_permutation
 
-/*
-Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
-for each 0 <= i < nums.length and return it.
-A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+// Task: https://leetcode.com/problems/build-array-from-permutation/
 
-Constraints:
-1 <= nums.length <= 1000
-0 <= nums[i] < nums.length
-The elements in nums are distinct.
-*/
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
+// for each 0 <= i < nums.length and return it.
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+// Constraints:
+// 1 <= nums.length <= 1000
+// 0 <= nums[i] < nums.length
+// The elements in nums are distinct.
+
 func buildArray(nums []int) []int {
 	res := make([]int, 0, len(nums))
 	for i := 0; i < len(nums); i++ {
