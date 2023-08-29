a6374ac154c0e34e26d234a44e993b901f9ff228
Filipp <sxexesx@gmail.com>
2023-08-29 12:55:50 +0300

    Update concat_arrays.go

--- concat_arrays/concat_arrays.go
+++ concat_arrays/concat_arrays.go
@@ -1,16 +1,16 @@
 package concat_arrays
 
-/*
-Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i]
-and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
-Specifically, ans is the concatenation of two nums arrays.
-Return the array ans.
+// Task: https://leetcode.com/problems/concatenation-of-array/
 
-Constraints:
-n == nums.length
-1 <= n <= 1000
-1 <= nums[i] <= 1000
-*/
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i]
+// and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+// Specifically, ans is the concatenation of two nums arrays.
+// Return the array ans.
+
+// Constraints:
+// n == nums.length
+// 1 <= n <= 1000
+// 1 <= nums[i] <= 1000
 func getConcatenation(nums []int) []int {
 	res := make([]int, 0, 2*len(nums))
 	for _, num := range nums {
